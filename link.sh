596cd9460c6eb7ff15efa3b6ef617c0bd7fa7edb
Austin Foxley <austinf@cetoncorp.com>
2012-09-25 16:46:14 -0700

    link gvimrc
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- link.sh
+++ link.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd
-rm -rf .vim .gitconfig .vimrc .bash_profile
+rm -rf .vim .gitconfig .vimrc .bash_profile .gvimrc
 ln -s dotfiles/.vim
 ln -s .vim/.vimrc
+ln -s .vim/.gvimrc
 ln -s dotfiles/.gitconfig
 ln -s dotfiles/.bash_profile
 
