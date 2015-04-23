3017fe3f1df7d67afbb4d7a4a329847c91f2c71a
Austin Foxley <austinf@cetoncorp.com>
2015-04-23 16:49:16 -0700

    add .ideavimrc
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- link.sh
+++ link.sh
@@ -3,9 +3,9 @@ cd
 rm -rf .vim .gitconfig .vimrc .bash_profile .gvimrc .profile .tmux.conf
 ln -s dotfiles/.vim
 ln -s .vim/.vimrc
+ln -s .vim/.ideavimrc
 ln -s .vim/.gvimrc
 ln -s dotfiles/.gitconfig
-ln -s dotfiles/.gitignore
 ln -s dotfiles/.bash_profile
 ln -s dotfiles/.bash_profile .profile
 ln -s dotfiles/.tmux.conf
